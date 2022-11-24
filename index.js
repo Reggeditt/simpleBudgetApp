ec66c630d42593563cf342efd45bfbb6768ce0ee
Reggeditt <tkodjosarso@gmail.com>
2022-11-24 22:36:02 +0000

    updates to js functionality

--- index.js
+++ index.js
@@ -12,27 +12,53 @@ const tdMisc = document.getElementById("misc-amt")
 const tdBills = document.getElementById("bills-amt")
 const tdIns = document.getElementById("ins-amt")
 const tdMed = document.getElementById("med-amt")
+const tdRentP = document.getElementById("rent")
+const tdFoodP= document.getElementById("food")
+const tdTransP = document.getElementById("trans")
+const tdUtilP = document.getElementById("util")
+const tdClothingP = document.getElementById("clothing")
+const tdSavingsP = document.getElementById("savings")
+const tdMiscP = document.getElementById("misc")
+const tdBillsP = document.getElementById("bills")
+const tdInsP = document.getElementById("ins")
+const tdMedP = document.getElementById("med")
 
-buttonEl.addEventListener("click", ()=>{
     
-    const income = +incomeElement.value;
-    const numDependants = +dependantsElement.value;
-    let dRS = 0;//dRS = dependant related spending
-    let gS = 0;//gs = general spending
-    if(numDependants<3){
-        dRS = 0.4*income;
-        gS = income - dRS;
+    
+    
+    if(window.localStorage.getItem('dRS')==="null"){
+        window.localStorage.setItem('dRS', '0');        
+        window.localStorage.setItem('gS', '0'); 
 
-    }else if(numDependants>=3&&numDependants<6){
-        dRS = 0.5*income;
-        gS = income - dRS;
-
-    }else{
-        dRS = 0.7*income;
-        gS = income - dRS;
     }
     
-    tdFood.innerText =`${dRS/5}`;
+    buttonEl.addEventListener("click", ()=>{    
+        const income = +incomeElement.value;
+        const numDependants = +dependantsElement.value;
+        let dRS =JSON.parse(window.localStorage.getItem('dRS'));        
+        let gS =JSON.parse(window.localStorage.getItem('gS'));        
+        // alert(`drs = ${dRS} and gs = ${gS}`)
+        if(numDependants<3){
+            dRS = 0.4*income;
+            gS = income - dRS;
+        }else if(numDependants>=3&&numDependants<6){
+            dRS = 0.5*income;
+            gS = income - dRS;
+    
+        }else{
+            dRS = 0.7*income;
+            gS = income - dRS;
+        }
+        window.localStorage.setItem('dRS', JSON.stringify(dRS));        
+        window.localStorage.setItem('gS', JSON.stringify(gS));        
+        
+        // alert(`drs = ${dRS} and gs = ${gS}`)
+        
+
+    })
+    let dRS =JSON.parse(window.localStorage.getItem('dRS'));        
+    let gS =JSON.parse(window.localStorage.getItem('gS'));
+    tdFood.innerText = `${dRS/5}`;
     tdMed.innerText = `${dRS/5}`;
     tdBills.innerText = `${dRS/5}`;
     tdTrans.innerText = `${dRS/5}`;
@@ -42,26 +68,15 @@ buttonEl.addEventListener("click", ()=>{
     tdIns.innerText = `${gS/5}`;
     tdSavings.innerText = `${gS/5}`;
     tdMisc.innerText = `${gS/5}`;
+    tdFoodP.innerText = `${(dRS/5)*100/(gS+dRS)}`;
+    tdMedP.innerText = `${(dRS/5)*100/(gS+dRS)}`;
+    tdBillsP.innerText = `${(dRS/5)*100/(gS+dRS)}`;
+    tdTransP.innerText = `${(dRS/5)*100/(gS+dRS)}`;
+    tdClothingP.innerText = `${(dRS/5)*100/(gS+dRS)}`;
+    tdRentP.innerText = `${(gS/5)*100/(gS+dRS)}`;
+    tdUtilP.innerText =`${(gS/5)*100/(gS+dRS)}`;
+    tdInsP.innerText = `${(gS/5)*100/(gS+dRS)}`;
+    tdSavingsP.innerText = `${(gS/5)*100/(gS+dRS)}`;
+    tdMiscP.innerText = `${(gS/5)*100/(gS+dRS)}`;
 
-    alert(`the income is ${income} and the dependants are ${numDependants}\n
-    \ncategory    \tamount \tpercentage\n
-            food\t${dRS/5}\t\n
-       transport\t${dRS/5}\t\t\n
-       medical\t${dRS/5}\t\t\n
-       clothing\t${dRS/5}\t\n
-       sch bills\t${dRS/5}\t\n
-    `)
-    
-    
-    
-    
-    
-    
-    // rent = ${gSar[0]}
-    // utility = ${gSar[1]}
-    // insurance = ${gSar[2]}
-    // savings = ${gSar[3]}
-    // investments = ${gSar[4]}
-    // `)
-
-})
+    // alert(`drs = ${dRS} and gs = ${gS}`)
